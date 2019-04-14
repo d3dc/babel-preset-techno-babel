45df36be394690f4e2a9317ea16ae9ec441d0dee
Sam Richard <sam.richard@gmail.com>
2019-04-13 22:55:26 -0600

    Add export namespace proposal

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ module.exports = () => ({
   plugins: [
     require("@babel/plugin-proposal-do-expressions"),
     require("@babel/plugin-proposal-export-default-from"),
+    require("@babel/plugin-proposal-export-namespace-from"),
     require("@babel/plugin-proposal-optional-chaining"),
     require("@babel/plugin-proposal-numeric-separator"),
     require("@babel/plugin-proposal-nullish-coalescing-operator"),
@@ -13,4 +14,4 @@ module.exports = () => ({
       }
     ]
   ]
-});
+})
